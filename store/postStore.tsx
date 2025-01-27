22b7d5604caf16ef18ebaeae6a7d304d9819118d
nagelk <nagelkilian05@gmail.com>
2025-01-27 23:38:41 +0100

    Feat: persistance du store

--- store/postStore.tsx
+++ store/postStore.tsx
@@ -5,6 +5,7 @@ import { getAuthenticatedUser } from "@/utils/utils";
 import { getPostsForChat } from '@/models/posts/posts.service';
 import { getMainChat } from '@/models/chats/chats.service';
 import { useEffect } from "react";
+import { persist } from 'zustand/middleware';
 
 interface PostData {
     posts: Post[] | null,
@@ -15,44 +16,47 @@ interface PostData {
 }
 
 // Create a store
-export const postStore = create<PostData>((set, get) => ({
-  posts: null as Post[] | null,
-  loaded: false, // Add a loaded flag
-  setPosts: (posts: Post[] | null) => set({ posts }),
-  loadData: async () => {
-    const { posts, loaded } = get();
+export const postStore = create<PostData>()(
+  persist(
+    (set, get) => ({
+      posts: null,
+      loaded: false,
+      setPosts: (posts: Post[] | null) => set({ posts }),
+      loadData: async () => {
+        const { posts, loaded } = get();
 
-    // Avoid re-fetching data if already loaded
-    if (loaded) return posts;
+        if (loaded) return posts;
 
-    // Fetch authenticated post
-    const authUser = await getAuthenticatedUser();
-    if (!authUser) return null;
+        const authUser = await getAuthenticatedUser();
+        if (!authUser) return null;
 
-    const chat = await getMainChat();
+        const chat = await getMainChat();
+        const postsData = await getPostsForChat(chat.id);
 
-    // Fetch full post data
-    const postsData = await getPostsForChat(chat.id);
-    if (postsData) {
-      set({ posts: postsData, loaded: true }); // Update both post and loaded flag
+        if (postsData) {
+          set({ posts: postsData, loaded: true });
+        }
+
+        return postsData;
+      },
+      reloadData: async () => {
+        const authUser = await getAuthenticatedUser();
+        if (!authUser) return;
+
+        const chat = await getMainChat();
+        const postsData = await getPostsForChat(chat.id);
+
+        if (postsData) {
+          set({ posts: postsData, loaded: true });
+        }
+      },
+    }),
+    {
+      name: 'post-storage', // Name of the local storage key
+      getStorage: () => localStorage, // Default storage is localStorage
     }
-
-    return postsData;
-  },
-  reloadData: async () => {
-    const authUser = await getAuthenticatedUser();
-    if (!authUser) return;
-
-    const chat = await getMainChat();
-    const postsData = await getPostsForChat(chat.id);
-
-    if (postsData) {
-      set({ posts: postsData, loaded: true });
-    }
-    const { posts, loaded } = get();
-    console.log(posts);
-  },
-}));
+  )
+);
 
 interface props {
     initialData: Post[]
