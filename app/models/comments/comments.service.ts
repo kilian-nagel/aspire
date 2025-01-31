93c646dfe6a852d8f4541a1a5f40de7b49b7e3cb
nagelk <nagelkilian05@gmail.com>
2025-01-31 23:48:31 +0100

    Fix: correction type params+ récupérations users associées à un commentaire

--- app/models/comments/comments.service.ts
+++ app/models/comments/comments.service.ts
@@ -2,16 +2,26 @@
 
 import { createClient } from "@/utils/supabase/server";
 
-export const addComment = async (userId: string, postId: string, content: string) => {
+export const addComment = async (userId: string, postId: number, content: string) => {
   const supabase = await createClient();
   const { data, error } = await supabase.from('comments').insert([{ user_id: userId, post_id: postId, content }]);
   if (error) throw error;
   return data;
 };
 
-export const getCommentsForPost = async (postId: string) => {
+export const modifyComment = async (commentId: number, content: string) => {
   const supabase = await createClient();
-  const { data, error } = await supabase.from('comments').select('*').eq('post_id', postId);
+  const { data, error } = await supabase.from('comments').update({content:content}).eq("id", commentId);
+  if (error) throw error;
+  return data;
+};
+
+export const getCommentsForPost = async (postId: number) => {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.from('comments')
+  .select('*, users:users(*)')
+  .eq('postId', postId);
   if (error) throw error;
   return data;
 };
