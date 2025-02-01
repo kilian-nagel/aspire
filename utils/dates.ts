38ca3f15587f8beba3cdd9a57b88bfa6f7f49836
nagelk <nagelkilian05@gmail.com>
2025-02-01 12:59:13 +0100

    Fix: affichage date au pluriel incorrect

--- utils/dates.ts
+++ utils/dates.ts
@@ -1,40 +1,41 @@
 export function timeAgo(dateString: string): string {
     const inputDate = new Date(dateString);
     const now = new Date();
+    let display_text = "";
 
     if (isNaN(inputDate.getTime())) {
         throw new Error("Invalid date format");
     }
 
     const diffInMs = now.getTime() - inputDate.getTime();
-    const diffInSeconds = Math.floor(diffInMs / 1000);
+    const one_second = 1000;
+    const one_minute = 60000;
+    const one_hour = 3600000;
+    const one_day = 86400000;
+    let time = 0;
 
-    if (diffInSeconds < 60) {
-        return `${diffInSeconds} seconds ago`;
+    console.log(diffInMs);
+
+    if(!(diffInMs >= one_minute)){
+        time = Math.floor(diffInMs/one_second);
+        display_text = `${time} second`;
+    } else if(!(diffInMs >= one_hour)){
+        time = Math.floor(diffInMs/one_minute);
+        display_text = `${time} minutes`;
+    } else if(!(diffInMs >= one_day)){
+        time = Math.floor(diffInMs/one_hour);
+        display_text = `${time} hour`;
+    } else {
+        const formattedDate = inputDate.toLocaleDateString("fr-FR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+        return formattedDate;
     }
 
-    const diffInMinutes = Math.floor(diffInSeconds / 60);
-    if (diffInMinutes < 60) {
-        return `${diffInMinutes} minutes ago`;
-    }
-
-    const diffInHours = Math.floor(diffInMinutes / 60);
-    if (diffInHours < 24) {
-        return `${diffInHours} hours ago`;
-    }
-
-    const diffInDays = Math.floor(diffInHours / 24);
-    if (diffInDays <= 2) {
-        return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
-    }
-
-    // Return the exact date if > 2 days ago
-    const formattedDate = inputDate.toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-    });
-    return formattedDate;
+    let plural = time > 1 ? "s":"";
+    return display_text + plural + " ago";
 }
