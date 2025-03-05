f713bb78bcbd66d0346e15664672d67342a2171f
nagelk <nagelkilian05@gmail.com>
2025-03-05 18:49:12 +0100

    Fix: suppression de la persistance des données du store des habitudes dans le localstorage

--- store/habitsStore.tsx
+++ store/habitsStore.tsx
@@ -4,7 +4,6 @@ import {create} from 'zustand';
 import {getAuthenticatedUser} from "@/utils/utils";
 import {getUserHabits} from '@/models/habits/habits.service';
 import {useEffect} from "react";
-import {persist} from 'zustand/middleware';
 
 interface HabitData {
     habits: Habit[] | null,
@@ -15,30 +14,25 @@ interface HabitData {
 
 // Create a store
 export const habitStore = create<HabitData>()(
-    persist(
-        (set, get) => ({
-            habits: null,
-            loaded: false,
-            setHabits: (habits: Habit[]) => set({habits}),
-            loadData: async () => {
+    (set, get) => ({
+        habits: null,
+        loaded: false,
+        setHabits: (habits: Habit[]) => set({habits}),
+        loadData: async () => {
 
-                // Fetch authenticated habit
-                const authUser = await getAuthenticatedUser();
-                if (!authUser) return null;
+            // Fetch authenticated habit
+            const authUser = await getAuthenticatedUser();
+            if (!authUser) return null;
 
-                // Fetch full habit data
-                const habitData = await getUserHabits(authUser.id);
-                if (habitData) {
-                    set({habits: habitData, loaded: true}); // Update both user and loaded flag
-                }
+            // Fetch full habit data
+            const habitData = await getUserHabits(authUser.id);
+            if (habitData) {
+                set({habits: habitData, loaded: true}); // Update both user and loaded flag
+            }
 
-                return habitData;
-            },
-        }),
-        {
-            name: 'habit-store', // Name for the persisted data in storage
-        }
-    )
+            return habitData;
+        },
+    })
 );
 
 interface props {
@@ -46,10 +40,12 @@ interface props {
 }
 
 export const HabitsStoreInitializer: React.FC<props> = ({initialData}) => {
+    console.log(initialData);
     const setHabits = habitStore((state) => state.setHabits);
 
     useEffect(() => {
         setHabits(initialData);
+        console.log("setuo habits");
     }, [initialData, setHabits]);
 
     return null;
