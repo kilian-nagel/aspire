87f763e5d1192cdc489e4f79eef57952a30d9f26
nagelk <nagelkilian05@gmail.com>
2025-03-06 20:21:09 +0100

    Feat: capacité de voir l'ensemble des habitudes ou les habitudes concernant aujourd'hui

--- components/habits/habits-cards.tsx
+++ components/habits/habits-cards.tsx
@@ -15,13 +15,13 @@ import {
 } from "@/components/ui/dialog"
 import {useRef, useState} from "react";
 
-export const HabitsCards = ({habits_type}: {habits_type: Tables<'habitCategory'>[]}) => {
+export const HabitsCards = ({habits_type, no_complete_action}: {habits_type: Tables<'habitCategory'>[], no_complete_action?: boolean}) => {
     const dialog_btn = useRef<HTMLButtonElement | null>(null);
-    const habits = habitStore((store) => store.habits);
+    const {habits} = habitStore();
     const habits_filtered_by_completion = filterHabitsByCompletion(habits);
     const [habit_to_edit, set_habit_to_edit] = useState<null | Habit>(null);
 
-    if (!habits) {
+    if (!habits || habits.length === 0) {
         return (<p>No habits...</p>)
     }
 
@@ -30,6 +30,26 @@ export const HabitsCards = ({habits_type}: {habits_type: Tables<'habitCategory'>
             set_habit_to_edit(habit);
             dialog_btn.current.click();
         }
+    }
+
+    if (no_complete_action) {
+        return (
+            <>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-6">
+                    {habits.map(habit => <HabitCard edit_habit_function={() => edit_habit(habit)} no_complete_action={true} key={habit.id} completed={true} {...habit} />)}
+                </div>
+
+                <Dialog >
+                    <DialogTrigger asChild className="absolute hidden">
+                        <Button ref={dialog_btn} variant="outline">New Habit</Button>
+                    </DialogTrigger>
+                    <DialogContent className="min-w-[1200px]">
+                        <DialogTitle></DialogTitle>
+                        <HabitForm habits_type={habits_type} habit={habit_to_edit ?? undefined} />
+                    </DialogContent>
+                </Dialog>
+            </>
+        );
     }
 
     return (
