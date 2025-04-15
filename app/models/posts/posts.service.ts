f1eb8f9ba498972d5301ed41d935874d9bce34bf
nagelk <nagelkilian05@gmail.com>
2025-04-15 07:11:53 +0200

    Feat: support pagination pour la méthode de récup des posts

--- app/models/posts/posts.service.ts
+++ app/models/posts/posts.service.ts
@@ -1,27 +1,44 @@
 "use server";
 
-import {createClient} from "@/utils/supabase/server";
-import {Post} from "@/models/posts/posts.types";
-import {check_if_content_is_unacceptable} from "@/utils/validation";
+import { createClient } from "@/utils/supabase/server";
+import { Post } from "@/models/posts/posts.types";
+import { check_if_content_is_unacceptable } from "@/utils/validation";
 
-export const createPost = async (post: {userId: string; chatId: number; content: string; id?: number}) => {
+export const createPost = async (post: {
+    userId: string;
+    chatId: number;
+    content: string;
+    id?: number;
+}) => {
     if (check_if_content_is_unacceptable(post.content)) {
-        throw Error('Post contains inappropriate content');
+        throw Error("Post contains inappropriate content");
     }
 
     const supabase = await createClient();
-    const {data, error} = await supabase.from('posts').insert([post]).single();
+    const { data, error } = await supabase
+        .from("posts")
+        .insert([post])
+        .single();
     if (error) throw error;
     return data;
 };
 
-export const modifyPost = async (post: {userId: string, content: string, postId: number}) => {
+export const modifyPost = async (post: {
+    userId: string;
+    content: string;
+    postId: number;
+}) => {
     if (check_if_content_is_unacceptable(post.content)) {
-        throw Error('Post contains inappropriate content');
+        throw Error("Post contains inappropriate content");
     }
 
     const supabase = await createClient();
-    const {data, error} = await supabase.from('posts').update({content: post.content}).eq("id", post.postId).is("postId", null).single();
+    const { data, error } = await supabase
+        .from("posts")
+        .update({ content: post.content })
+        .eq("id", post.postId)
+        .is("postId", null)
+        .single();
     if (error) throw error;
     return data;
 };
@@ -29,25 +46,30 @@ export const modifyPost = async (post: {userId: string, content: string, postId:
 export const deletePost = async (postId: number) => {
     const supabase = await createClient();
 
-    const {data, error} = await supabase.from('posts').delete().eq("id", postId);
+    const { data, error } = await supabase
+        .from("posts")
+        .delete()
+        .eq("id", postId);
     if (error) throw error;
     return data;
 };
 
 export const getPostsForChat = async (chatId: number) => {
     const supabase = await createClient();
-    const {data, error} = await supabase
-        .from('posts')
-        .select(`
+    const { data, error } = await supabase
+        .from("posts")
+        .select(
+            `
       *,
       user:users(*),         
       likes:likes(*),        
       shares:shares(*),      
-      comments:posts(*)`) // Correctly fetch related comments
-        .eq('chatId', chatId)
+      comments:posts(*)`,
+        ) // Correctly fetch related comments
+        .eq("chatId", chatId);
 
     if (error) {
-        console.error('Error fetching posts:', error);
+        console.error("Error fetching posts:", error);
         throw error;
     }
 
@@ -60,20 +82,33 @@ export const getPostsForChat = async (chatId: number) => {
 };
 
 // Fetch all posts with associated likes, shares, and users
-export const getAllPosts = async (): Promise<Post[]> => {
+export const getAllPosts = async (
+    lastTimeStamp: string | null = null,
+    window: number = 20,
+): Promise<Post[]> => {
     const supabase = await createClient();
-    const {data, error} = await supabase
-        .from('posts')
-        .select(`
+
+    const query = supabase
+        .from("posts")
+        .select(
+            `
       *,
       user:users(*),         
       likes:likes(*),        
       shares:shares(*),      
       comments:posts(*)  
-    `).is("postId", null);
+    `,
+        )
+        .is("postId", null);
+
+    if (lastTimeStamp) {
+        query.lt("created_at", lastTimeStamp);
+    }
+
+    const { data, error } = await query.limit(window);
 
     if (error) {
-        console.error('Error fetching posts:', error);
+        console.error("Error fetching posts:", error);
         throw error;
     }
 
@@ -85,23 +120,24 @@ export const getAllPosts = async (): Promise<Post[]> => {
     }));
 };
 
-
 export const getPost = async (id: number): Promise<Post> => {
     const supabase = await createClient();
-    const {data, error} = await supabase
-        .from('posts')
-        .select(`
+    const { data, error } = await supabase
+        .from("posts")
+        .select(
+            `
             *,
             user:users(*),         
             likes:likes(*),        
             shares:shares(*),      
             comments:posts(*)  
-            `)
+            `,
+        )
         .eq("id", id)
         .single();
 
     if (error) {
-        console.error('Error fetching posts:', error);
+        console.error("Error fetching posts:", error);
         throw error;
     }
 
