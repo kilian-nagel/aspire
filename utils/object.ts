56a57e02fb731b9c7e0c769e89b4137b6268aa41
nagelk <nagelkilian05@gmail.com>
2025-04-19 07:26:09 +0200

    Feat: méthode util pour merge deux objects

---
+++ utils/object.ts
@@ -0,0 +1,20 @@
+interface ObjectWithId {
+    id: string | number;
+}
+
+export function merge_data(data1: ObjectWithId[], data2: ObjectWithId[]) {
+    // On récupère les ids du tableau n°1.
+    const ids: { [key: string]: number } = {};
+    for (const item of data1) {
+        ids[item.id.toString()] = 1;
+    }
+
+    // On ajoute les éléments du tableau 2 au tableau 1 seulement s'ils n'ont pas déjà été ajoutés
+    const final_items = data1;
+    for (const item of data2) {
+        // On n'ajoute pas éléments avec une id déjà existante
+        if (!ids[item.id]) final_items.push(item);
+    }
+
+    return final_items;
+}
